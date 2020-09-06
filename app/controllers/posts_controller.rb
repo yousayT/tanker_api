b267354310d61dba3f6ab04e9d3e12831e503e4a
yousay <yousay19981009@gmail.com>
2020-09-07 00:34:32 +0900

    add tag function in posts#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 class PostsController < ApplicationController
 
   def create
-    puts @current_user
+    # puts @current_user
+    #postmanチェック済み（2020/09/07）
     @post = Post.new(content: params[:content], user_id: @current_user.id)
+    #フロントからタグの名前の配列が来ることを想定（例：　tag_name: ["tag1", "tag2", ... , "tagn"]）
+    tag_names = params.permit(tag_name:[])
+    tag_names[:tag_name].each do |tag_name|
+      @post.tag_list.add(tag_name)
+    end
     @post.save
-    render json: @post
+    render json: {
+        post: @post
+      }
   end
 
   def show
