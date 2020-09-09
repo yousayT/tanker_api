9e77e7def135e22bdba2ea00e4e39cccb50accae
yousay <yousay19981009@gmail.com>
2020-09-10 00:27:21 +0900

    fix follows#create and follows#destroy

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,24 +2,35 @@ class FollowsController < ApplicationController
   before_action :authenticate_user
 
   def create
-    #postmanチェック済み（2020/08/24）
+    #postmanチェック済み（2020/09/10）
     @follow = Follow.new(follower_id: @current_user.id, followee_id: params[:id])
     # current_userのフォロー数を1増やす
-    User.find_by(id: @current_user.id).followee_count += 1
+    @current_user.followee_count += 1
+    @current_user.save
     # フォローされたユーザのフォロワー数を1増やす
-    User.find_by(id: params[:id]).follower_count +=1
+    followee = User.find_by(id: params[:id])
+    followee.follower_count += 1
+    followee.save
     @follow.save
-    render json: @follow
+    render json: {
+      user: @current_user
+    }
   end
 
   def destroy
-    #postmanチェック済み(2020/08/24)
+    #postmanチェック済み(2020/09/10)
     @follow = Follow.find_by(follower_id: @current_user.id, followee_id: params[:id])
     # current_userのフォロー数を1減らす
-    User.find_by(id: @current_user.id).followee_count -= 1
+    @current_user.followee_count -= 1
+    @current_user.save
     # フォローされていたユーザのフォロワー数を1減らす
-    User.find_by(id: params[:id]).follower_count -= 1
+    followee = User.find_by(id: params[:id])
+    followee.follower_count -= 1
+    followee.save
     @follow.destroy
+    render json: {
+      user: @current_user
+    }
   end
 
   def follower_index
