2de567fc44a6a47e76ad3a81eefdfc798ae5e94f
yousay <yousay19981009@gmail.com>
2020-09-14 23:23:58 +0900

    fix relation between posts and likes, and add validates to content

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ApplicationRecord
+  # タグ機能の追加
   acts_as_taggable
-  
-  belongs_to :user
-  has_many :likes
 
-  validates :content, {presence: true}
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :users, through: :likes
+
+  validates :content, {
+    presence: true,
+    length: {maximum: 50}
+  }
   validates :user_id, {presence: true}
 end
