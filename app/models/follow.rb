bd0e31d408f9c3d3f0ce2dce15ca65f3b87d9993
yousay <yousay19981009@gmail.com>
2020-09-14 23:25:35 +0900

    fix relation between follows and users, and add validations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,13 @@
 class Follow < ApplicationRecord
+  belongs_to :following_user, class_name: "User", foreign_key: :follower_id
+  belongs_to :followed_user, class_name: "User", foreign_key: :followee_id
+
   validates :follower_id, {presence: true}
-  validates :followee_id, {presence: true}
+  validates :followee_id, {
+    presence: true,
+    # 同じフォロー情報は1つまでしか存在できない
+    uniqueness: {scope: :follower_id, message: "そのユーザは既にフォローしています"},
+    # フォロワーとフォローされている人のidは必ず異なる
+    numericality: {other_than: :follower_id}
+  }
 end
