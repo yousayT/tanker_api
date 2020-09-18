d9317f19c3f6209ab7e9967ea1b13a73b942215a
yousay <yousay19981009@gmail.com>
2020-09-18 22:20:51 +0900

    add key in redis_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,7 @@ if Rails.env.production?
   # 本番環境ではredisをsessionの保持に使う
   Rails.application.config.session_store :redis_store,{
     servers: ENV['REDIS_URL'],
+    key: "session",
     expire_after: 1.week
   }
 
