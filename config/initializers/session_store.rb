bd041372630d89fd8a9da85414fc7b2c23b8f1a2
yousay <yousay19981009@gmail.com>
2020-09-18 23:05:04 +0900

    add []

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 if Rails.env.production?
   # 本番環境ではredisをsessionの保持に使う
   Rails.application.config.session_store :redis_store,{
-    servers: ENV['REDIS_URL'] + "/0/session",
+    servers: [ENV['REDIS_URL'] + "/0/session"],
     expire_after: 1.week
   }
 
