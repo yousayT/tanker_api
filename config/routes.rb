1d40698500d31aea9ef0224be79a9ccf0fe01821
yousay <yousay19981009@gmail.com>
2020-09-21 00:12:00 +0900

    add namespace 'admin' and rewrite routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
   namespace 'api' do
-    post "users/create" => "users#create"
-    get "users/:id" => "users#show"
-    post "users/:id/update" => "users#update"
     post "login" => "users#login"
     post "logout" => "users#logout"
-    post "users/:id/destroy" => "users#destroy"
+    resources :users, only: [:create, :show, :update, :destroy]
 
     post "users/:id/follow" => "follows#create"
     post "users/:id/remove" => "follows#destroy"
     get "users/:id/follower" => "follows#follower_index"
     get "users/:id/followee" => "follows#followee_index"
 
-    post "posts/create" => "posts#create"
     get "posts/timeline" => "follows#timeline"
-    get "posts/:id" => "posts#show"
-    post "posts/:id/destroy" => "posts#destroy"
+    resources :posts, only: [:create, :show, :destroy]
     post "posts/:id/like" => "posts#like"
     post "posts/:id/unlike" => "posts#unlike"
+
+    namespace 'admin' do
+      resources :users, only: [:index]
+    end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
