bf986868072e9f326707bc7941b5628cc322934c
yousay <yousay19981009@gmail.com>
2020-10-12 00:01:00 +0900

    add validates to dm model

---
+++ app/models/dm.rb
@@ -0,0 +1,8 @@
+class Dm < ApplicationRecord
+  belongs_to :sender, class_name: "User", foreign_key: :sender_id
+  belongs_to :receiver, class_name: "User", foreign_key: :receiver_id
+
+  validates :content, {presence: true, length: {maximum: 500}}
+  validates :sender_id, {presence: true}
+  validates :receiver_id, {presence: true} 
+end
