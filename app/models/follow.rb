eb56693eba9e0aaec274e7b07e2ba5c898ac94ae
yousay <yousay19981009@gmail.com>
2020-10-12 00:02:03 +0900

    fix relation between users and follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,6 @@
 class Follow < ApplicationRecord
-  belongs_to :following_user, class_name: "User", foreign_key: :follower_id
-  belongs_to :followed_user, class_name: "User", foreign_key: :followee_id
+  belongs_to :follower, class_name: "User", foreign_key: :follower_id
+  belongs_to :followee, class_name: "User", foreign_key: :followee_id
 
   validates :follower_id, {presence: true}
   validates :followee_id, {
