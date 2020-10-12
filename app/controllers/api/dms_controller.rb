0f43abe34e9f96168d96c0117ce6a9a0955a4562
yousay <yousay19981009@gmail.com>
2020-10-13 04:03:26 +0900

    fix dms#user_index and dms#dm_index adding function of checking unread

--- app/controllers/api/dms_controller.rb
+++ app/controllers/api/dms_controller.rb
@@ -2,21 +2,29 @@ class Api::DmsController < ApplicationController
   before_action :authenticate_user
   before_action :check_user, only: :destroy
 
-  # 過去にDMを送ったことのあるユーザの一覧と最も最近のDMの内容を返す
+  # 過去にDMを送ったことのあるユーザの一覧と最も最近のDMの内容、未読数を返す
   def user_index
     has_contacted_user_ids = Dm.where(sender_id: @current_user.id).group(:receiver_id).pluck(:receiver_id)
     has_contacted_users = Array.new
     has_contacted_user_ids.each do |has_contacted_user_id|
-      has_contacted_users.push(Dm.where(sender_id: @current_user.id, receiver_id: has_contacted_user_id)).order('created_at DESC').limit(1)
+      has_contacted_user = fetch_infos_from_dm(Dm.where(sender_id: @current_user.id, receiver_id: has_contacted_user_id).order('created_at DESC').limit(1))
+      unread_count = Dm.where(sender_id: @current_user.id, receiver_id: has_contacted_user_id, is_read: false).count
+      has_contacted_user.store("unread_count", unread_count)
+      has_contacted_users.push(has_contacted_user)
     end
     render json: {
-      users: fetch_infos_from_dm(has_contacted_users)
+      users: has_contacted_users
     }
   end
 
-  # あるユーザとのDMの内容を全て返す
+  # あるユーザとのDMの内容を送信者を明らかにして返し、全ての未読を既読にする
   def dm_index
     dms = Dm.where(sender_id: @current_user.id, receiver_id: params[:receiver_id]).or(Dm.where(sender_id: params[:receiver_id], receiver_id: @current_user.id)).order('created_at DESC')
+    unread_dms = Dm.where(sender_id: params[:receiver_id], receiver_id: @current_user.id, is_read: false)
+    unread_dms.each do |unread_dm|
+      unread_dm.is_read = true
+      unread_dm.save
+    end
     molded_dms = Array.new
     # もしcurrent_userがDMの送信者ならtrueを、受信者ならfalseを加えてDMのid、内容、作成時とともに返す
     dms.each do |dm|
