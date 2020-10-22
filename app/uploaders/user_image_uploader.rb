e981823579fa504f5cc7148daa2b54bf753ec3a2
yousay <yousay19981009@gmail.com>
2020-10-23 04:15:47 +0900

    fix default url

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -11,7 +11,7 @@ class UserImageUploader < CarrierWave::Uploader::Base
     #   # For Rails 3.1+ asset pipeline compatibility:
     #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     #
-      "images/fallback/" + [version_name, "default.png"].compact.join('_')
+      "https://tanker-user-image.s3.amazonaws.com/images/fallback/" + [version_name, "default.png"].compact.join('_')
     end
   else
     storage :file
